c800c652a821afe17ea5d9f23c3ef8f9682d4062
ManiGeek <maniweeb@gmail.com>
2023-07-06 13:15:01 +0330

    feat:NewReleases

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import Home from './pages/home/Home' 
 import GenresMoods from './pages/browser/genres-moods/GenresMoods'
+import NewReleases from './pages/browser/new-releases/NewReleases'
+
 let routes = [
     {path: '/', element: <Home />},
     {path: '/browse', element: <GenresMoods />},
+    {path: '/NewReleases', element: <NewReleases />},
 ]
 
 export default routes
