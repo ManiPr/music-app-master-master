f840931cc7dfabb9fd699c4d07fa0a878a33d814
ManiGeek <maniweeb@gmail.com>
2023-07-26 11:50:04 +0330

    MusicComponent
    Edit Music

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -5,25 +5,16 @@ import heart from '../../assets/images/SVG File/heart.svg'
 import more from '../../assets/images/SVG File/more.svg'
 import play from '../../assets/images/SVG File/Play.svg'
 export default function Music({props}) {
+  import React from 'react'
+import './Music.css'
+
+import heart from '../../assets/images/SVG File/heart.svg'
+import more from '../../assets/images/SVG File/more.svg'
+import play from '../../assets/images/SVG File/Play.svg'
+export default function Music({props}) {
+{console.log(require('../../assets/images/Musics/LP5.png'));}
   return (
     <>
-  {props.map((prop) => (
-        <div className='music center'>
-        <div className='music__wrapper'>
-            <div className='music__container-image'>
-            <img className='music__image'  src={require(`../../assets/images/Musics/${props.Image}`)} alt='Playlist' />
-            </div>
-     
-        <div className='music__information center'>
-        <p className='music__name'>
-        {props.Name}
-        </p>
-        <p className='music__creator'>Like Count</p>
-        </div>
-        </div>
-    
-    </div>
-    ))}
        <div className='music center'>
         <div className='music__wrapper'>
             <div className='music__container-image'>
@@ -46,4 +37,6 @@ export default function Music({props}) {
       </div>
     </>
   );
+}
+
 }
